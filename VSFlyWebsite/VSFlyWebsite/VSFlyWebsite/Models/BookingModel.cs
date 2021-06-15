37aff4ec4e9278119e5aa08d5720447ce6f934f8
d-roduit <daniel@roduit.com>
2021-06-15 17:47:58 +0200

    Add models to Website and API

---
+++ VSFlyWebsite/VSFlyWebsite/VSFlyWebsite/Models/BookingModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VSFlyWebsite.Models
+{
+    public class BookingModel
+    {
+        public FlightModel FlightModel { get; set; }
+        public List<PassengerModel> PassengerModelList { get; set; }
+    }
+}
---
+++ VSFlyWebsite/VSFlyWebsite/VSFlyWebsite/Models/FlightModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace VSFlyWebsite.Models
+{
+    public class FlightModel
+    {
+        public int FlightNo { get; set; }
+        public String Departure { get; set; }
+        public String Destination { get; set; }
+        public DateTime Date { get; set; }
+        public double BasePrice { get; set; }
+        public double SalePrice { get; set; }
+    }
+}
